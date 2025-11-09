4d1ea5439a99b06429f540f3d7b895cd3c6c2fc6
sudowski27 <Sudowski@o2.pl>
2025-11-09 15:05:29 +0100

    Remove method hello

--- src/main/java/com/example/demo/TaskManagerApplication.java
+++ src/main/java/com/example/demo/TaskManagerApplication.java
@@ -9,9 +9,4 @@ public class TaskManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagerApplication.class, args);
 	}
-
-    // @GetMapping("/hello")
-    // public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      // return String.format("Hello %s!", name);
-    // }
 }
