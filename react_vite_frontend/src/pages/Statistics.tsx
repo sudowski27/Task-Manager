888a301ec50d55dcea406ba567abfcb63388e7cd
sudowski27 <Sudowski@o2.pl>
2026-06-07 17:12:26 +0200

    Update file react_vite_frontend/src/pages/Statistics.tsx:
    Add button Back to Home

--- react_vite_frontend/src/pages/Statistics.tsx
+++ react_vite_frontend/src/pages/Statistics.tsx
@@ -1,10 +1,13 @@
 import BarUsagePlot from '../components/BarUsagePlot.tsx'
+import { useNavigate } from 'react-router-dom';
+
 type StatisticsProps = {
   isDark: boolean;
   setIsDark: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 export default function Statistics({isDark, setIsDark}: StatisticsProps) {
+    const navigate = useNavigate()
     return (
         <div className="static">
 
@@ -24,6 +27,27 @@ export default function Statistics({isDark, setIsDark}: StatisticsProps) {
                 onClick={() => setIsDark(v => !v)}
             >
                 Change theme
+            </button>
+
+            <button
+                className="
+                bg-blue-600
+                text-white
+                py-2
+                px-4
+                rounded-md
+                hover:bg-gray-800
+                transition
+                "
+                style={{
+                    backgroundColor: "var(--button-bg)",
+                color: "var(--text-color)"
+                }}
+                onMouseEnter={(e) => (e.currentTarget.style.backgroundColor = "var(--button-hover)")}
+                onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = "var(--button-bg)")}
+                onClick={() => navigate("/")}
+            >
+                Back to Home
             </button>
 
             <div className="fixed top-30 left-20 size-100">
